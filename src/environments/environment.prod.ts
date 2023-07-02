c01439e44af99ded47f86507754d8e829a368dfe
sakthi <sakthi@localhost.localdomain>
2023-07-02 16:05:40 +0530

    Pushing all files and folders

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  api: {
+    server: 'https://nithira.glitch.me/',
+    mapKey: 'cfd205e8-e6db-4cfc-a91a-38bd72162f26',
+    googleAuthClientId: '445066017363-2udrc4mrq6u0qh44207ejt51fk4bp0f6.apps.googleusercontent.com'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  api: {
+    server: 'http://localhost:8000/',
+    mapKey: 'cfd205e8-e6db-4cfc-a91a-38bd72162f26',
+    googleAuthClientId: '445066017363-2udrc4mrq6u0qh44207ejt51fk4bp0f6.apps.googleusercontent.com'
+  }
+};
+// https://nithira-backend.onrender.com/
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
